c3c6df0372a8c392e5d3a524de20a8e25d1e996c
Srujan <srujan_barai@yahoo.com>
2015-10-24 21:35:06 +0530

    Mid-change safe push.

--- app/src/main/java/example/srujan/com/drinkwater/DailyDetails.java
+++ app/src/main/java/example/srujan/com/drinkwater/DailyDetails.java
@@ -1,12 +1,14 @@
 package example.srujan.com.drinkwater;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by Srujan on 20-09-2015.
  */
 public class DailyDetails implements Serializable {
-    Date date;
+    Calendar calendar;
     int volume;
+    int target;
 }
